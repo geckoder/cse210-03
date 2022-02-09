d9c392d2d72fb60d911de1238109acc1577e5e93
Emily <cas20037@byui.edu>
2022-02-09 14:00:01 -0700

    Winner class

---
+++ winner.py
@@ -0,0 +1,11 @@
+from secret_word import SecretWord
+
+
+class Winner():
+    "Determines if player wins or loses the Jumper Game"
+
+    def __init__(self):
+        self._winning = True
+
+    def win(self):
+        pass
