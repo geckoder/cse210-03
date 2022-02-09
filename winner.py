daca7e804276d21c37e3ac302e7e3a989d5dcf9d
Emily <cas20037@byui.edu>
2022-02-09 15:18:11 -0700

    winner file

--- winner.py
+++ winner.py
@@ -1,11 +1,22 @@
 from secret_word import SecretWord
+from wordHandler import ProcessWord
+from jumper import Jumper
 
 
 class Winner():
     "Determines if player wins or loses the Jumper Game"
 
     def __init__(self):
-        self._winning = True
+        self.complete_word = ProcessWord()
+        self.right_word = SecretWord()
+        self.parachute = Jumper()
 
     def win(self):
-        pass
+        "Prints statements according to winner or looser condition"
+        # Winner condition
+        if self.complete_word.secretWord == self.right_word.random_word:
+            print('You guessed the word! Have a safe landing!')
+
+        # Looser condition
+        elif self.parachute._list == '  x  ':
+            print('Your parachute is gone! Call the ambulance!')
